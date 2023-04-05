1445db9bfc7a426bf09ea78814f4e3ffbcba50e5
malayadaniGIT <109133343+malayadaniGIT@users.noreply.github.com>
2023-04-05 17:48:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+function myFunction() {
+  document.getElementById("box").style.backgroundColor = "red";
+}
+function myFunction2() {
+  document.getElementById("box").style.backgroundColor = "green";
+}
